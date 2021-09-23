f894bb0cb7005b55a97de72df267694f80412c97
Natália <nataliadelimac@outlook.com>
2021-09-23 15:56:47 -0300

    Animação de click com lottie

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
+import React, {Fragment, useState } from 'react';
 import ReactDOM from 'react-dom';
+import styled from 'styled-components';
+import Lottie from 'react-lottie';
+import heart from  './heart.json'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const ButtonWrapper = styled.button`
+  --size: 100px;
+  width: var(--size);
+  height: var(--size);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 33px;
+  background-color: #fff;
+  border: 0;
+  padding: 0;
+  cursor: pointer;
+  outline: 0;
+  border-radius: 100%;
+
+  .animation{
+    pointer-events: none;
+  }
+`;
+console.log(heart)
+export const LikeButton = () =>{
+  const [isLiked, setisLike]= useState(false);
+  const [animationState, setAnimationState] = useState({
+    isStopped: true, isPaused: false,
+    direction: -1,
+  });
+
+  const defaultOptions = {
+    loop: false,
+    autoplay: false, 
+    animationData: heart,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  };
+
+  return (
+    <Fragment>
+      <ButtonWrapper onClick={()=>{
+        const reverseAnimation = -1;
+        const normalAnimation = 1;
+        setAnimationState({
+          ...animationState,
+          isStopped: false,
+          direction : animationState.direction === normalAnimation
+            ? reverseAnimation
+            :normalAnimation,
+        })
+        setisLike(!isLiked);
+      }}>
+          <div className="animation">
+          <Lottie 
+            options={defaultOptions}
+            height={400}
+            width={400}
+            direction={animationState.direction}
+            isStopped={animationState.isStopped}
+            isPaused={animationState.isPaused}/>
+          </div>
+         
+      </ButtonWrapper>
+        <span>
+          {isLiked ? 1: 0}
+        </span>
+    </Fragment>
+      );
+};
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <LikeButton />
+  </div>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
